fe1c131943592c995a419b297ef222127d435a82
remi <duplanremi@gmail.com>
2023-09-25 23:02:07 -0400

    more routes

--- src/app/mock-api/apps/routes-definition/routes.ts
+++ src/app/mock-api/apps/routes-definition/routes.ts
@@ -1,7 +1,10 @@
 
 
 export class Routes {
+   
+    public static  LOGIN : string = 'user_app/customuser/';
 
-    public static  LOGIN : string = 'login';
+    public static  GET_USERS_CONFIGURATION : string = 'user/user_configurations';
+    public static  GET_USER_CONFIGURATION : string = 'user/user_configurations/<int:pk>/';
 
 }
